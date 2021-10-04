b69d717a26d460996b7189ee8318cb641df556f2
Andre <andre2656@gmail.com>
2021-10-03 19:07:42 -0600

    Initial commit 10-03-2021

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/ble', function () {
+Route::get('/app', function () {
     return view('dredirect');
 });
